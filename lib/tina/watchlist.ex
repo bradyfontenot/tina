4758f6a15d6bb58998d0990288405ac8fd2261c3
Brady Fontenot <bfont52@gmail.com>
2020-07-02 16:57:29 -0500

    Update typespecs

--- lib/tina/watchlist.ex
+++ lib/tina/watchlist.ex
@@ -12,21 +12,23 @@ defmodule Tina.Watchlist do
     :updated_at
   ]
 
+  @spec list_all() :: tuple()
   def list_all() do
     Alpaca.get_data(@endpoint, struct(Tina.Watchlist))
   end
 
+  @spec get_by_id(String.t()) :: tuple()
   def get_by_id(id) do
     path = "#{@endpoint}/#{id}"
     Alpaca.get_data(path, struct(Tina.Watchlist))
   end
 
-  @spec create(map()) :: struct()
+  @spec create(map()) :: tuple()
   def create(params = %{name: _, symbols: _}) do
     Alpaca.post_data(@endpoint, params, struct(Tina.Watchlist))
   end
 
-  @spec create(map()) :: struct()
+  @spec create(map()) :: tuple()
   def create(params = %{name: _}) do
     Alpaca.post_data(@endpoint, params, struct(Tina.Watchlist))
   end
@@ -36,36 +38,40 @@ defmodule Tina.Watchlist do
     {:error, "invalid arguments"}
   end
 
-  # @spec update(map()) :: struct()
+  @spec update(String.t(), map()) :: tuple()
   def update(id, params = %{name: _, symbols: _}) do
     path = "#{@endpoint}/#{id}"
     Alpaca.put_data(path, params, struct(Tina.Watchlist))
   end
 
-  # @spec update(map()) :: struct()
+  @spec update(String.t(), map()) :: tuple()
   def update(id, params = %{name: _}) do
     path = "#{@endpoint}/#{id}"
     Alpaca.put_data(path, params, struct(Tina.Watchlist))
   end
 
-  # @spec update(any()) :: tuple()
+  @spec update(any(), any()) :: tuple()
   def update(_, _) do
     {:error, "invalid arguments"}
   end
 
+  @spec add_asset(String.t(), map()) :: tuple()
   def add_asset(watchlist_id, symbol) do
     path = "#{@endpoint}/#{watchlist_id}"
     params = %{symbol: symbol}
     Alpaca.post_data(path,params, struct(Tina.Watchlist))
   end
 
+  @spec delete_asset(String.t(), map()) :: tuple()
+  def delete_asset(id, symbol) do
+    path = "#{@endpoint}/#{id}/#{symbol}"
+    Alpaca.delete_data(path)
+  end
+
+  @spec delete_list(String.t()) :: tuple()
   def delete_list(id) do
     path = "#{@endpoint}/#{id}"
     Alpaca.delete_data(path)
   end
 
-  def delete_asset(id, symbol) do
-    path = "#{@endpoint}/#{id}/#{symbol}"
-    Alpaca.delete_data(path)
-  end
 end
