b8f182151df3b39f11398b04288dd4c3b7362a72
Brady Fontenot <bfont52@gmail.com>
2020-07-11 17:56:38 -0500

    Add docs & typespec

--- lib/tina/clock.ex
+++ lib/tina/clock.ex
@@ -1,20 +1,42 @@
 defmodule Tina.Clock do
-  use Tesla
+  @moduledoc """
+  Returns the current market timestamp, whether or not the market is currently open,
+  as well as the times of the next market open and close
+
+  Available functions:
+  get/0
+
+  Alpaca Markets Clock Documentation:
+  https://alpaca.markets/docs/api-documentation/api-v2/clock/
+  """
+
+  # TODO for initial release:
+  # Add return value desc to docmodule above
+  # write tests
+
   alias Tina.Alpaca
 
   @endpoint "clock"
 
-  @doc """
-  Calendar struct matches k,v from /account endpoint
-  """
+  @type t :: %__MODULE__{
+          is_open: boolean(),
+          next_close: String.t(),
+          next_open: String.t(),
+          timestamp: String.t()
+        }
+
   defstruct [
-    :timestamp,
     :is_open,
     :next_open,
-    :next_close
+    :next_close,
+    :timestamp
   ]
 
-  def get_clock() do
+  @doc """
+  get/0 returns the market clock.
+  """
+  @spec get() :: tuple()
+  def get() do
     Alpaca.get_data(@endpoint, struct(Tina.Clock))
   end
 end
