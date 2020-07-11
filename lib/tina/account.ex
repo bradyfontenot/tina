b764cffa8890f1263567a2c7d1f5d1ede0bb91c2
Brady Fontenot <bfont52@gmail.com>
2020-07-11 17:53:24 -0500

    Add docs. move activity & history to own modules

--- lib/tina/account.ex
+++ lib/tina/account.ex
@@ -1,11 +1,23 @@
 defmodule Tina.Account do
-  use Tesla
+  @moduledoc """
+Returns important information related to an account,
+including account status, funds available for trade, funds available
+for withdrawal, and various flags relevant to an account’s ability to trade.
+
+  Available functions:
+  `get/0`
+
+  Alpaca Markets Account Documentation:
+  https://alpaca.markets/docs/api-documentation/api-v2/account
+  """
+
+  # TODO for initial release:
+  # Add return value desc to docmodule above
+  # write tests
+
   alias Tina.Alpaca
 
-  @account_endpoint "account"
-  @activity_endpoint "account/activities"
-  @config_endpoint "account/configurations"
-  @portfolio_history_endpoint "account/portfolio/history"
+  @endpoint "account"
 
   @type t :: %__MODULE__{
           account_blocked: boolean,
@@ -65,153 +77,12 @@ defmodule Tina.Account do
     :transfers_blocked
   ]
 
-  defmodule TradeActivity do
-    @doc """
-    TradeActivity struct matches json object structure returned
-    from /account/activity_type endpoint for TradeActivity types.
-
-    """
-
-    @type t :: %__MODULE__{
-            activity_type: String.t(),
-            id: String.t(),
-            cum_qty: String.t(),
-            leaves_qty: String.t(),
-            price: String.t(),
-            qty: String.t(),
-            side: String.t(),
-            symbol: String.t(),
-            transaction_time: String.t(),
-            order_id: String.t(),
-            type: String.t()
-          }
-
-    defstruct [
-      :activity_type,
-      :id,
-      :cum_qty,
-      :leaves_qty,
-      :price,
-      :qty,
-      :side,
-      :symbol,
-      :transaction_time,
-      :order_id,
-      :type
-    ]
-  end
-
-  defmodule NonTradeActivity do
-    @doc """
-     NonTradeActivity struct matches json object structure returned
-     from /account/activity_type endpoint for NonTradeActivity types.
-    """
-
-    @type t :: %__MODULE__{
-            activity_type: String.t(),
-            id: String.t(),
-            date: String.t(),
-            net_amount: String.t(),
-            symbol: String.t(),
-            qty: String.t(),
-            per_share_amount: String.t()
-          }
-
-    defstruct [
-      :activity_type,
-      :id,
-      :date,
-      :net_amount,
-      :symbol,
-      :qty,
-      :per_share_amount
-    ]
-  end
-
-  defmodule Config do
-    @type t :: %__MODULE__{
-            dtbp_check: String.t(),
-            no_shorting: boolean(),
-            suspend_trade: boolean(),
-            trade_confirm_email: String.t(),
-            # not documented in Alpaca docs
-            pdt_check: String.t()
-          }
-
-    defstruct [
-      :dtbp_check,
-      :no_shorting,
-      :suspend_trade,
-      :trade_confirm_email,
-      :pdt_check
-    ]
-  end
-
-  defmodule PortfolioHistory do
-    @type t :: %__MODULE__{
-            base_value: integer(),
-            equity: list(float()),
-            profit_loss: list(float()),
-            profit_loss_pct: list(float()),
-            timeframe: String.t(),
-            timestamp: list(integer())
-          }
-
-    defstruct [
-      :base_value,
-      :equity,
-      :profit_loss,
-      :profit_loss_pct,
-      :timeframe,
-      :timestamp
-    ]
-  end
-
+  @doc """
+  `get/0` returns all Account Info.
+  """
   @spec get() :: tuple()
   def get() do
-    Alpaca.get_data(@account_endpoint, struct(Tina.Account))
-  end
-
-  @spec get_activity() :: tuple()
-  def get_activity() do
-    Alpaca.get_data(@activity_endpoint)
-  end
-
-  @doc """
-    retrieving a single activity type: activity_type = "activity"
-    retrieving multiple types in one call:  activity_type = "type1, type2, type3, ..."
-  """
-  @spec get_activity_by_type(String.t()) :: tuple()
-  def get_activity_by_type(activity_type) do
-    Alpaca.get_data(@activity_endpoint, activity_type: activity_type)
-  end
-
-  @spec get_config() :: tuple()
-  def get_config() do
-    Alpaca.get_data(@config_endpoint, struct(Config))
-  end
-
-  @doc """
-    params supplied as map
-  """
-  @spec update_config(map()) :: tuple()
-  def update_config(params) do
-    Alpaca.patch_data(@config_endpoint, params, struct(Config))
-  end
-
-  @doc """
-    query_params must be supplied as keyword list
-    valid params:
-      period: <string>
-      timeframe: <string>
-      date_start: <string> "YYYY-MM-DD" (omitted in Alpaca Docs)
-      date_end: <string> "YYYY-MM-DD"
-      extended_hours: <bool>
-  """
-
-  @spec get_portfolio_history() :: tuple()
-  def get_portfolio_history(query \\ []) do
-    Alpaca.get_data(@portfolio_history_endpoint, query, struct(PortfolioHistory))
+    Alpaca.get_data(@endpoint, struct(Tina.Account))
   end
 
 end
